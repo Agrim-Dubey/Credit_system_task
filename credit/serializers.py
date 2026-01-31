571c9b70acfdba008f2f0b6e9ee6c4faa591152b
Agrim <agrimdubey37@gmail.com>
2026-01-31 21:43:31 +0530

    Implemented the registration endpoint

--- credit/serializers.py
+++ credit/serializers.py
@@ -10,8 +10,20 @@ class CustomerRegisterInputSerializer(serializers.ModelSerializer):
     
     def create(self,validated_data):
         monthly_salary=validated_data["monthly_salary"]
-        approved_limitt = 36*monthly_salary
+        approved_limit = 36*monthly_salary
         
-        customer = CustomerData.objects.create(**validated_data,approved_limit = approved_limitt,current_debt=0)
+        customer = CustomerData.objects.create(**validated_data,approved_limit = approved_limit,current_debt=0)
         return customer
         
+class CustomerRegisterOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerData
+        fields = [
+            "id",            
+            "first_name",
+            "last_name",
+            "age",
+            "phone_number",
+            "monthly_salary",
+            "approved_limit",
+        ]
--- credit/views.py
+++ credit/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializers import CustomerRegisterInputSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import CustomerRegisterInputSerializer,CustomerRegisterOutputSerializer
 # Create your views here.
 
 
 class RegisterCustomer(APIView):
     def post(self,request):
-        # serializer = CustomerRegisterInputSerializer(data=request.data)
-        return None
+        serializer = CustomerRegisterInputSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        customer  =serializer.save()
+        response_serializer = CustomerRegisterOutputSerializer(customer)
+        return Response(response_serializer.data,staus=status.HTTP_201_CREATED)
