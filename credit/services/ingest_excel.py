282e074f6a113a27ceb20a3de3d91206eca5ac47
Agrim <agrimdubey37@gmail.com>
2026-01-31 18:37:12 +0530

    Ingested the file:loan data and completed the ingestion task

--- credit/services/ingest_excel.py
+++ credit/services/ingest_excel.py
@@ -1,10 +1,11 @@
+from decimal import Decimal
 import pandas as pd
 from pathlib import Path
-from credit.models import CustomerData
+from credit.models import CustomerData, LoanData
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 CUSTOMER_FILE = BASE_DIR / "data" / "customer_data.xlsx"
-
+LOAN_FILE = BASE_DIR / "data" / "loan_data.xlsx"
 
 def ingest_customers():
     df = pd.read_excel(CUSTOMER_FILE)
@@ -23,6 +24,43 @@ def ingest_customers():
                 "monthly_salary": int(row["Monthly Salary"]),
                 "approved_limit": int(row["Approved Limit"]),
                 "current_debt": 0,
+            },
+        )
+
+        if is_created:
+            created += 1
+        else:
+            skipped += 1
+
+    return {
+        "created": created,
+        "skipped": skipped,
+        "total": len(df),
+    }
+
+
+def ingest_loans():
+    df = pd.read_excel(LOAN_FILE)
+
+    created = 0
+    skipped = 0
+
+    for _, row in df.iterrows():
+        customer = CustomerData.objects.get(
+            customer_id=int(row["Customer ID"])
+        )
+
+        _, is_created = LoanData.objects.get_or_create(
+            customer=customer,
+            loan_id=int(row["Loan ID"]),
+            defaults={
+                "loan_amount": int(row["Loan Amount"]),
+                "tenure": int(row["Tenure"]),
+                "interest_rate": Decimal(str(row["Interest Rate"])),
+                "monthly_repayment": int(row["Monthly payment"]),
+                "EMIs_paid_on_time": int(row["EMIs paid on Time"]),
+                "start_date": pd.to_datetime(row["Date of Approval"]).date(),
+                "end_date": pd.to_datetime(row["End Date"]).date(),
             },
         )
 
--- credit/tasks.py
+++ credit/tasks.py
@@ -1,13 +1,26 @@
 from celery import shared_task
-
-from celery import shared_task
-from credit.services.ingest_excel import ingest_customers
+from credit.services.ingest_excel import ingest_customers,ingest_loans
 
 
 @shared_task(bind=True)
 def ingest_customers_task(self):
     try:
         result = ingest_customers()
+        return {
+            "status": "success",
+            "result": result,
+        }
+    except Exception as e:
+        return {
+            "status": "failed",
+            "error": str(e),
+        }
+
+
+@shared_task(bind=True)
+def ingest_loans_task(self):
+    try:
+        result = ingest_loans()
         return {
             "status": "success",
             "result": result,
